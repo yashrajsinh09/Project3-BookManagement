aa66e7759e7c51563b7523f57fdb8241b9b711b5
shivamp2001 <shivampenshnwar2001@gmail.com>
2022-11-28 22:20:55 +0530

    some changes

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -7,32 +7,18 @@ const objectId = mongoose.Schema.Types.ObjectId;
 
 const bookSchema = new mongoose.Schema(
    {
-   title : {type : String, required : true,trim : true, unique : true},
-   excerpt : {type : String, required : true },
-   userId : {type : objectId,required : true,unique : true,ref : "User"},
-   ISBN : {type : String,required : true,trim : true,unique : true},
-   category : {type : String,required : true,trim : true },
-   subcategory : {
-    type : [ String ],
-    required : true,
-   },
-   reviews : {
-    type : Number,
-    default : 0,
-   },
-   deletedAt : {
-    type : Date
-   },
-   isDeleted : {
-    type : Boolean,
-    default : false
-   },
-   releasedAt : {
-    type : Date,
-    required : true,
-   }
+      title: { type: String, required: true, trim: true, unique: true },
+      excerpt: { type: String, required: true },
+      userId: { type: objectId, required: true, ref: "User" },
+      ISBN: { type: String, required: true, trim: true, unique: true },
+      category: { type: String, required: true, trim: true },
+      subcategory: { type: [String], required: true, },
+      reviews: { type: Number, default: 0, },
+      deletedAt: { type: Date },
+      isDeleted: { type: Boolean, default: false },
+      releasedAt: { type: Date, required: true, }
 
-}, {timestamps: true})
+   }, { timestamps: true })
 
 //__________________________ Exporting Book Schema ___________________________________________
 
